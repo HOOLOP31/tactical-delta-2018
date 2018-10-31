89bf885fbebb088e1411e0e22f48eaea9f3619b4
PlauM <p.moulin@student.isartdigital.com>
2018-10-31 17:38:18 +0100

    1e test de requete de la db

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// ---------- VARIABLES ---------- //
 var express    = require('express');
 var app        = express();
 var bodyParser = require("body-parser");
@@ -8,26 +9,72 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(port, () => {
+var mongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://<dbuser>:<dbpassword>@ds141783.mlab.com:41783/tactical-delta-2018-db';
+var dbName = 'tactical-delta-2018-db';
+
+/* - Collections - */
+var db;
+var users;
+var levels;
+
+
+// ------------------------------
+// ---------- INITIALIZATION ---------- //
+app.listen(port, function () => {
   console.log('Server listening at port %d', port);
 });
 
+mongoClient.connect(url, init);
+
+function init (err, client)
+{
+  db = client.db(dbName);
+  //users = db.collection('UserCollectionName');
+  levels = client.db('T_LEVELS');
+}
+
+
 // ------------------------------
 // ---------- REQUESTS ---------- //
-var levelName;
+
+
+
+// ------------------------------
+// ---------- TESTS ---------- //
+app.post('/getLevelInfos', function(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  
+  levels.find({name:level1});
+  console.log(req.body);
+  /*levelName = req.body.levelName;
+  size = {
+    x : req.body.sizeX,
+    y : req.body.sizeY
+  }*/
+  
+  levels.find().toArray(function (err, document){
+    res.end(JSON.stringify(document));
+  });
+  
+  //res.send('Name : ' + levelName + ' // Size : ' + size.x + ', ' + size.y);
+});
+
+
+// ------------------------------
+// ---------- EXEMPLES ---------- //
+/*var levelName;
 var size;
 app.post('/setLevel', function(req, res) {
-    console.log(req.body);
-    levelName = req.body.levelName;
-    size = {
-        x : req.body.sizeX,
-        y : req.body.sizeY
-    }
-  	res.send('Name : ' + levelName + ' // Size : ' + size.x + ', ' + size.y);
+  console.log(req.body);
+  levelName = req.body.levelName;
+  size = {
+    x : req.body.sizeX,
+    y : req.body.sizeY
+  }
+  res.send('Name : ' + levelName + ' // Size : ' + size.x + ', ' + size.y);
 });
 
 app.get('/getLevel', function(req, res) {
-  	res.send('Name : ' + levelName + ' // Size : ' + size.x + ', ' + size.y);
-});
-
-//1e tests
+  res.send('Name : ' + levelName + ' // Size : ' + size.x + ', ' + size.y);
+});*/
