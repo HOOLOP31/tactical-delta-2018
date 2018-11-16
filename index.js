a83a7599a1fd3db92909d0ac401375a7f59ab023
PlauM <p.moulin@student.isartdigital.com>
2018-11-16 17:18:27 +0100

    request pour recup un test de bdd

--- index.js
+++ index.js
@@ -17,67 +17,347 @@ var dbName = 'tactical-delta-2018-db';
 var db;
 var samplesCollection;
 var levelsCollection;
+var usersCollection;
 
 
 // ------------------------------
 // ---------- INITIALIZATION ---------- //
 app.listen(port, () => {
-  console.log('Server listening at port %d', port);
+    console.log('Server listening at port %d', port);
 });
 
 mongoClient.connect(url, { useNewUrlParser: true }, init);
 
 function init (err, client)
 {
-  db = client.db(dbName);
-  samplesCollection = db.collection('T_SAMPLES');
-  levelsCollection = db.collection('T_LEVELS');
+    db = client.db(dbName);
+    samplesCollection = db.collection('T_SAMPLES');
+    usersCollection = db.collection('T_USERS');
+    levelsCollection = db.collection('T_LEVELS');
 }
 
 
 // ------------------------------
 // ---------- REQUESTS ---------- //
-app.get('/', function(req, res) {
-  res.send("Server is running");
+app.get('/', function(req, res)
+{
+    res.send("Server is running");
 });
+
+/*app.get("/hasAnEmail", function(req, res)
+{
+    console.log("REQUEST :: /hasAnEmail");
+    
+    users.find({login:req.body.login}).toArray(function (err, document)
+    {
+        if(document.length > 0)
+        {
+            res.end(JSON.stringify(document[0]));
+        }
+        else
+        {
+            var lNewUser =
+            {
+                "login": req.body.login,
+                "hardcurrencies": 1000,
+                "softcurrencies": 10000,
+                "progress":
+                [
+                    {
+                        "area": 1,
+                        "unlocked": true,
+                        "levels":
+                        [
+                            {
+                                "levelId": 1,
+                                "unlocked": true,
+                                "score": 0,
+                                "stars": 0
+                            },
+                            {
+                                "levelId": 2,
+                                "unlocked": false,
+                                "score": 0,
+                                "stars": 0
+                            },
+                            {
+                                "levelId": 3,
+                                "unlocked": false,
+                                "score": 0,
+                                "stars": 0
+                            },
+                            {
+                                "levelId": 4,
+                                "unlocked": false,
+                                "score": 0,
+                                "stars": 0
+                            },
+                            {
+                                "levelId": 5,
+                                "unlocked": false,
+                                "score": 0,
+                                "stars": 0
+                            }
+                        ]
+                    },
+                    {
+                        "area": 2,
+                        "unlocked": false,
+                        "levels":
+                        [
+                            {
+                                "levelId": 1,
+                                "unlocked": false,
+                                "score": 0,
+                                "stars": 0
+                            },
+                            {
+                                "levelId": 2,
+                                "unlocked": false,
+                                "score": 0,
+                                "stars": 0
+                            },
+                            {
+                                "levelId": 3,
+                                "unlocked": false,
+                                "score": 0,
+                                "stars": 0
+                            },
+                            {
+                                "levelId": 4,
+                                "unlocked": false,
+                                "score": 0,
+                                "stars": 0
+                            },
+                            {
+                                "levelId": 5,
+                                "unlocked": false,
+                                "score": 0,
+                                "stars": 0
+                            }
+                        ]
+                    },
+                    {
+                        "area": 3,
+                        "unlocked": false,
+                        "levels":
+                        [
+                            {
+                                "levelId": 1,
+                                "unlocked": false,
+                                "score": 0,
+                                "stars": 0
+                            },
+                            {
+                                "levelId": 2,
+                                "unlocked": false,
+                                "score": 0,
+                                "stars": 0
+                            },
+                            {
+                                "levelId": 3,
+                                "unlocked": false,
+                                "score": 0,
+                                "stars": 0
+                            },
+                            {
+                                "levelId": 4,
+                                "unlocked": false,
+                                "score": 0,
+                                "stars": 0
+                            },
+                            {
+                                "levelId": 5,
+                                "unlocked": false,
+                                "score": 0,
+                                "stars": 0
+                            }
+                        ]
+                    },
+                    {
+                        "area": 4,
+                        "unlocked": false,
+                        "levels":
+                        [
+                            {
+                                "levelId": 1,
+                                "unlocked": false,
+                                "score": 0,
+                                "stars": 0
+                            },
+                            {
+                                "levelId": 2,
+                                "unlocked": false,
+                                "score": 0,
+                                "stars": 0
+                            },
+                            {
+                                "levelId": 3,
+                                "unlocked": false,
+                                "score": 0,
+                                "stars": 0
+                            },
+                            {
+                                "levelId": 4,
+                                "unlocked": false,
+                                "score": 0,
+                                "stars": 0
+                            },
+                            {
+                                "levelId": 5,
+                                "unlocked": false,
+                                "score": 0,
+                                "stars": 0
+                            }
+                        ]
+                    },
+                    {
+                        "area": 5,
+                        "unlocked": false,
+                        "levels":
+                        [
+                            {
+                                "levelId": 1,
+                                "unlocked": false,
+                                "score": 0,
+                                "stars": 0
+                            },
+                            {
+                                "levelId": 2,
+                                "unlocked": false,
+                                "score": 0,
+                                "stars": 0
+                            },
+                            {
+                                "levelId": 3,
+                                "unlocked": false,
+                                "score": 0,
+                                "stars": 0
+                            },
+                            {
+                                "levelId": 4,
+                                "unlocked": false,
+                                "score": 0,
+                                "stars": 0
+                            },
+                            {
+                                "levelId": 5,
+                                "unlocked": false,
+                                "score": 0,
+                                "stars": 0
+                            }
+                        ]
+                    },
+                    {
+                        "area": 6,
+                        "unlocked": false,
+                        "levels":
+                        [
+                            {
+                                "levelId": 1,
+                                "unlocked": false,
+                                "score": 0,
+                                "stars": 0
+                            },
+                            {
+                                "levelId": 2,
+                                "unlocked": false,
+                                "score": 0,
+                                "stars": 0
+                            },
+                            {
+                                "levelId": 3,
+                                "unlocked": false,
+                                "score": 0,
+                                "stars": 0
+                            },
+                            {
+                                "levelId": 4,
+                                "unlocked": false,
+                                "score": 0,
+                                "stars": 0
+                            },
+                            {
+                                "levelId": 5,
+                                "unlocked": false,
+                                "score": 0,
+                                "stars": 0
+                            }
+                        ]
+                    }
+                ],
+                "items":
+                [
+                    {
+                        "name": "item1",
+                        "amount": 0
+                    },
+                    {
+                        "name": "item2",
+                        "amount": 0
+                    }
+                ]
+            };
+            users.insertOne(lNewUser);
+            res.end(JSON.stringify(lNewUser));
+        }
+    });
+});*/
 
 
 // ------------------------------
 // ---------- TESTS ---------- //
-app.post("/getSampleData", function(req, res){
-  samplesCollection.find({"string":req.body.myString}).toArray(function(err, document){
+app.post("/getSampleData", function(req, res)
+{
+  console.log("REQUEST :: /getSampleData");
+  
+  samplesCollection.find({"string":req.body.myString}).toArray(function(err, document)
+  {
     res.send(document);
   });
 });
 
+app.post("/getTestData", function(req, res)
+{
+  console.log("REQUEST :: /getTestData");
+  
+  samplesCollection.find({"thisString":req.body.myString}).toArray(function(err, document)
+  {
+    //res.send(document);
+    //res.send(document[0]);
+    //res.send(JSON.stringify(document[0].length));
+    res.send(JSON.stringify(document[0]));
+  });
+});
 
-
-app.post("/getLevel", function (req,res){
+app.post("/getLevel", function (req,res)
+{
+  console.log("REQUEST :: /getLevel");
+  
+  levelsCollection.find({"name":req.body.levelName}).toArray(function(err, document)
+  {
+    res.send(document);// Stoppe la requete et revoie 1 seule valeur en parametre
     
-    levelsCollection.find({"name":req.body.levelName}).toArray(function(err, document){
-      res.send(document);// Stoppe la requete et revoie 1 seule valeur en parametre
-        /*if(document.length != 0)
-        {
-            var lCurrentHighestScore = parseFloat(JSON.stringify(document[0].progress[lNumberPlanet-1].levels[lNumberLevel-1].score));
-            
-            if(lScore > lCurrentHighestScore){
-                
-                lIndex = lNumberLevel - 1;
-                updateVal["progress.$.levels." + lIndex + ".score"] = lScore;
-                
-                users.findOneAndUpdate({
-                    "login":req.query.login, 
-                    "progress.planet":lNumberPlanet, 
-                    "progress.levels.level":lNumberLevel},
-                    {$set: updateVal},
-                    {upsert:true, returnOriginal:false});
-                res.end(JSON.stringify(lScore)); 
-            }
-            else{
-                res.end("false");
-            }
-        }*/
-    });
+    /*if(document.length != 0)
+    {
+      var lCurrentHighestScore = parseFloat(JSON.stringify(document[0].progress[lNumberPlanet-1].levels[lNumberLevel-1].score));
+      if(lScore > lCurrentHighestScore)
+      {
+        lIndex = lNumberLevel - 1;
+        updateVal["progress.$.levels." + lIndex + ".score"] = lScore;
+        users.findOneAndUpdate({
+          "login":req.body.login, 
+          "progress.planet":lNumberPlanet, 
+          "progress.levels.level":lNumberLevel},
+          {$set: updateVal},
+          {upsert:true, returnOriginal:false});
+        res.end(JSON.stringify(lScore)); 
+      }
+      else{
+          res.end("false");
+      }
+    }*/
+  });
 });
 
 
